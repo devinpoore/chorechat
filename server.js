3f2d46dce1d4070d72f1c2ff07c0b971c0e13fe8
Dalinar Kholin <devinpoore@users.noreply.github.com>
2019-12-03 17:52:31 -0800

    added test log every minute

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const schedule = require("node-schedule");
 
 const http = require("http");
-
 const https = require("https");
 
 require("dotenv").config();
@@ -23,7 +22,6 @@ const db = require("./models");
 // deployed mongo db connection
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://" + process.env.mongoUser + ":" + process.env.mongoPW + "@ds251158.mlab.com:51158/heroku_5npspkxg";
 
-// TODO: Look into {'useUnifiedTopology': true} - shows up in Heroku deploy logs
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true }).then(() => {
     console.log("\nMongoose successfully connected to chorechat db\n");
 }).catch(err => {
@@ -60,15 +58,7 @@ app.get("*", (req,res) => {
 const scheduleFunctions = require("./scheduleFunctions.js");
 const chores = require("./chores.js");
 
-// --------------------------------------------------------
-
-// const alertChoreRule = new schedule.RecurrenceRule();
-// alertChoreRule.dayOfWeek = 1;
-// alertChoreRule.hour = 13;
-// alertChoreRule.minute = 10;
-
-// const alertChore = schedule.scheduleJob(alertChoreRule, scheduleFunctions.sendInitialChoreAlert(client));
-
+// Send out the initial weekly chore duties to each roommate
 const alertChores = schedule.scheduleJob({dayOfWeek: 2, hour: 8, minute: 30}, () => {
     db.Roomie.find({}).then(dbRes => {
         for (roomie of dbRes) {
@@ -83,20 +73,23 @@ const alertChores = schedule.scheduleJob({dayOfWeek: 2, hour: 8, minute: 30}, ()
     }).catch(err => console.log(err));
 });
 
-const testAlert = schedule.scheduleJob({dayOfWeek: 2, hour: 16, minute: 45}, () => {
-    db.Roomie.find({"name": "Devin"}).then(dbDevin => {
-        console.log("Testing...\n");
-        console.log(dbDevin);
-        for (roomie of dbDevin) {
-            twilioClient.messages.create({
-                body: "testing...\n\n-1301 Chorechat",
-                from: process.env.twilioNum,
-                to: roomie.phoneNumber
-            }).then(message => console.log(message));
-        }
-    });
+// Just testing
+const testAlert = schedule.scheduleJob({dayOfWeek: 2, hour: 18, second: 45}, () => {
+    // db.Roomie.find({"name": "Devin"}).then(dbDevin => {
+    //     console.log("Testing...\n");
+    //     console.log(dbDevin);
+    //     for (roomie of dbDevin) {
+    //         twilioClient.messages.create({
+    //             body: "testing...\n\n-1301 Chorechat",
+    //             from: process.env.twilioNum,
+    //             to: roomie.phoneNumber
+    //         }).then(message => console.log(message));
+    //     }
+    // });
+    console.log("\ntest alert firing every minute...\n");
 });
 
+//
 // const testAlert2 = schedule.scheduleJob({dayOfWeek: 2, hour: 16, minute: 31}, () => {
 //     db.Roomie.find({"name": "Devin"}).then(dbDevin => {
 //         console.log(dbDevin);
@@ -110,14 +103,7 @@ const testAlert = schedule.scheduleJob({dayOfWeek: 2, hour: 16, minute: 45}, () 
 //     });
 // });
 
-// ---------------------------------------------------------
-
-// const adjustChoreRule = new schedule.RecurrenceRule();
-// adjustChoreRule.dayOfWeek = 1;
-// adjustChoreRule.hour = 6;
-
-// const updateChores = new schedule.scheduleJob(adjustChoreRule, scheduleFunctions.adjustChores);
-
+// Rotate the chore duty list each Monday before the new chore notification is sent
 const updateChores = new schedule.scheduleJob({dayOfWeek: 1, hour: 6, minute: 30}, () => {
     db.Roomie.find({}).then(dbUpdateRes => {
         console.log(dbUpdateRes);
@@ -136,14 +122,7 @@ const updateChores = new schedule.scheduleJob({dayOfWeek: 1, hour: 6, minute: 30
     }).catch(err => console.log(err));
 });
 
-// ---------------------------------------------------------
-
-// const choreReminderRule = new schedule.RecurrenceRule();
-// choreReminderRule.dayOfWeek = [0, 4];
-// choreReminderRule.hour = 8;
-
-// const choreReminders = new schedule.scheduleJob(choreReminderRule, scheduleFunctions.determineReminder);
-
+// Send sms reminder to any roommate who hasn't completed their chore on Thursday and Sunday
 const choreReminders = new schedule.scheduleJob({dayOfWeek: [0, 4], hour: 8, minute: 30}, () => {
     db.Roomie.find({"choreComplete": false}).then(dbReminderRes => {
         // console.log(dbReminderRes);
